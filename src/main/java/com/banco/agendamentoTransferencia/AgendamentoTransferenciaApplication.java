6e77701919a402b7edcb75a0968ed24d968e33c0
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2025-02-18 17:32:09 -0300

    Estrutura inicial da API

---
+++ src/main/java/com/banco/agendamentoTransferencia/AgendamentoTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.banco.agendamentoTransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTransferenciaApplication.class, args);
+	}
+
+}
